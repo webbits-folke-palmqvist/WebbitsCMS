3b48725b082d10dc45139584522ad8b50ee45f2f
Folke Palmqvist <folke.palmqvist@webbits.nu>
2013-01-07 00:02:18 +0100

    La till funktion att ta bort, ändra eller lägga till

--- Hemsida/admin/pages.php
+++ Hemsida/admin/pages.php
@@ -5,26 +5,26 @@ if(!$sub){
 	?>
 	<a class="btn" href="?page=Pages&sub=add">Lägg till en sida</a>
 	<hr>
+	<?php success(); ?>
 	<table class="table table-hover">
 		<tr>
-			<td style="width:1px;"><strong>#</strong></td>
 			<td><strong>Namn</strong></td>
 			<td style="width:1%;"><strong>Ändra</strong></td>
 			<td style="width:11%;"><strong>Ta bort</strong></td>
 		</tr>
 		<?php
-		$result = mysql_query("SELECT * FROM pages WHERE deleted = '0', name != '404', name != 'contact' ORDER BY name");
+		$result = mysql_query("SELECT * FROM pages WHERE deleted = '0' ORDER BY name");
 
 		while($row = mysql_fetch_array($result)){
 			?>
 			<tr>
-			<td><?php echo $row['id']; ?></td>
-			<td><a href="#"><?php echo $row['name']; ?></a></td>
-			<td><a class="btn" href="#">Ändra</a></td>
+			<td><a target="_Blank" href="../?page=<?php echo $row['name']; ?>"><?php echo $row['name']; ?></a></td>
+			<td><a class="btn" href="?page=Pages&sub=edit&id=<?php echo $row['id']; ?>">Ändra</a></td>
 			<?php
-			if($row['name'] != "Hem" OR $row['name'] != "404"){
+			if($row['name'] == "Hem" OR $row['name'] == "404"){
+			} else {
 				?>
-				<td><a class="btn" href="#">Ta bort</a></td>
+				<td><a class="btn" href="../process.php?action=pages&do=delete&id=<?php echo $row['id']; ?>">Ta bort</a></td>
 				<?php
 			}
 			?>
@@ -38,7 +38,8 @@ if(!$sub){
 	if($sub == "add"){
 		?>
 		<div class="pages-add">
-			<form action="../process.php&action=pages&do=add" method="POST">
+			<form action="../process.php?action=pages&do=add" method="POST">
+				<?php error(); ?>
 				<input class="input-fill" type="text" name="title" placeholder="Titel"><br />
 				<textarea name="content"></textarea>
 				<script language="javascript" type="text/javascript" src="../assets/tiny_mce/tiny_mce.js"></script>
@@ -50,11 +51,41 @@ if(!$sub){
 			    });
 			    </script>
 			    <br>
-			    <?php success(); ?>
-				<a class="btn btn-danger" href="?page=Kategori&action=view&cat_id=<?php echo secure($_GET['cat_id']); ?>">Avbryt</a> <input class="btn btn-success" type="submit" value="Spara">
+				<a class="btn btn-danger" href="?page=Pages">Avbryt</a> <input class="btn btn-success" type="submit" value="Lägg till sidan">
 			</form>
 		</div>
 		<?php
+	} elseif ($sub == "edit"){
+		$id = secure($_GET['id']);
+		
+		if(!$id){
+			header('location: ?page=Pages');
+		} else {
+			$result = mysql_query("SELECT * FROM pages WHERE deleted = '0' AND id = '$id' LIMIT 1");
+
+			while($row = mysql_fetch_array($result)){
+				?>
+				<div class="pages-add">
+					<form action="../process.php?action=pages&do=edit" method="POST">
+						<?php error(); ?>
+						<input class="input-fill" type="text" name="title" placeholder="Titel" value="<?php echo $row['name'] ?>"><br />
+						<textarea name="content"><?php echo $row['content'] ?></textarea>
+						<input type="hidden" name="id" id="id" value="<?php echo $row['id'] ?>">
+						<script language="javascript" type="text/javascript" src="../assets/tiny_mce/tiny_mce.js"></script>
+					    <script language="javascript" type="text/javascript">
+					    tinyMCE.init({
+					            theme : "advanced",
+					            mode : "textareas",
+					            theme_advanced_toolbar_location : "top"
+					    });
+					    </script>
+					    <br>
+						<a class="btn btn-danger" href="?page=Pages">Avbryt</a> <input class="btn btn-success" type="submit" value="Spara ändringar">
+					</form>
+				</div>
+				<?php
+			}
+		}
 	}
 }
 ?>
