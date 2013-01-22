79609b9560457a57ffb6f63eea0c268fd4557683
Folke Palmqvist <folke.palmqvist@webbits.nu>
2013-01-22 14:49:57 +0100

    Ändrade texten på startsidan på adminpanelen

--- Hemsida/admin/start.php
+++ Hemsida/admin/start.php
@@ -1,4 +1,4 @@
 <?php $Check->login(); ?>
 <h3>Välkommen</h3>
 <p>Detta är ett CMS byggt utav <a target="_Blank" href="http://webbits.nu/">Webbits</a> för deras webbdesign kunder.</p>
-<p><strong>Har du något problem med vårt CMS?</strong><br> - skapa ett ärende på ditt konto hos webbits genast och förklara så hjälper vi dig så snabbt vi kan.</p>
+<p><strong>Har du något problem med vårt CMS?</strong><br> - <a href="http://billing.webbits.nu/submitticket.php?step=2&deptid=7&subject=CMS">Skapa ett ärende</a> på ditt konto hos Webbits genast och förklara vad som är problemet så hjälper vi dig så snabbt vi kan.</p>
